0a8de886681afe8c3b79a2bd68541395c35fe294
Santiago Seguro <santi@Santiagos-MacBook-Pro.local>
2025-07-22 22:55:01 -0300

    actualización de la página

---
+++ src/data/videos.ts
@@ -0,0 +1,223 @@
+// └─ data/videos.ts
+
+export interface Video {
+  libraryId: string
+  videoId: string
+  format: 'long' | 'short'
+  category: string
+}
+
+export const videos: Video[] = [
+  {
+    libraryId: '471301',
+    videoId: 'a128a7b9-7106-4074-bcdb-1af6aac33d29',
+    format: 'long',
+    category: 'documentary',
+  },
+  {
+    libraryId: '471301',
+    videoId: '16fe78e7-eef9-4c67-ac49-a734d6b832f3',
+    format: 'short',
+    category: 'animation finance',
+  },
+  {
+    libraryId: '471301',
+    videoId: '2f4ce6a2-7529-49d1-b0a9-8f5f2edb7e6a',
+    format: 'short',
+    category: 'animation finance',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'd7a902db-9f7f-4246-996f-199564bd8510',
+    format: 'short',
+    category: 'animation finance',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'cd7d04c2-3dce-488a-a008-6b0e37a4c719',
+    format: 'long',
+    category: 'vsl finance animation',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'e8199e45-3bac-49ef-ace2-423384ac08e6',
+    format: 'long',
+    category: 'animation finance',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'e9ce66ab-49e7-4d37-a856-73a9807d80d9',
+    format: 'long',
+    category: 'animation talking-head',
+  },
+  {
+    libraryId: '471301',
+    videoId: '4ccf9e99-7802-4c2f-bee6-fb5d03cca279',
+    format: 'long',
+    category: 'animation finance',
+  },
+   {
+    libraryId: '471301',
+    videoId: 'e952a455-bf0b-455c-a7ca-c65dd295abf9',
+    format: 'short',
+    category: 'gaming',
+  },
+  {
+    libraryId: '471301',
+    videoId: '99068628-3afc-4adf-994a-1bf20e8d480f',
+    format: 'short',
+    category: 'gaming',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'ddc5c146-e452-4aa5-9d2d-233bd754ea7e',
+    format: 'short',
+    category: 'gaming',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'a075d625-824b-4a04-8ea2-86408d351033',
+    format: 'long',
+    category: 'talking-head',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'bab261d7-f0fb-4d79-891d-10e14697e03f',
+    format: 'long',
+    category: 'brand',
+  },
+  {
+    libraryId: '471301',
+    videoId: '4fdc4dc8-f2a6-40fc-bef4-1a09d19ee2d5',
+    format: 'long',
+    category: 'brand',
+  },
+  {
+    libraryId: '471301',
+    videoId: '56b19785-eb67-4591-bbfe-f58a35d7740d',
+    format: 'long',
+    category: 'gaming',
+  },
+  {
+    libraryId: '471301',
+    videoId: '5284c47d-63ff-4770-bd43-e3af3abcc2b5',
+    format: 'long',
+    category: 'gaming',
+  },
+  {
+    libraryId: '471301',
+    videoId: '9d5500a2-9a05-4cce-b585-cbf53ac6b9f1',
+    format: 'long',
+    category: 'gaming',
+  },
+  {
+    libraryId: '471301',
+    videoId: '1f76060d-da8e-487c-b8b9-359a58b43880',
+    format: 'short',
+    category: 'talking-head',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'aa001ec5-a536-469e-8840-21e40bf690a8',
+    format: 'short',
+    category: 'talking-head',
+  },
+  {
+    libraryId: '471301',
+    videoId: '7c6c8f93-0827-4e89-bfcb-ddeef63e09b7',
+    format: 'short',
+    category: 'talking-head',
+  },
+   {
+    libraryId: '471301',
+    videoId: 'c527d809-bb4b-41b2-a9da-9f463824bf6e',
+    format: 'long',
+    category: 'finance talking-head',
+  },
+  {
+    libraryId: '471301',
+    videoId: '7ff9d166-4875-421c-83b7-52a9d0c992dc',
+    format: 'long',
+    category: 'finance talking-head',
+  },
+  {
+    libraryId: '471301',
+    videoId: '4299ce4f-15ac-4e7d-a319-a8ce450749d5',
+    format: 'long',
+    category: 'finance talking-head',
+  },
+  {
+    libraryId: '471301',
+    videoId: '1a8ad29e-1496-426a-abb2-c69c825d3758',
+    format: 'short',
+    category: 'podcast',
+  },
+  {
+    libraryId: '471301',
+    videoId: '9865cf75-3b49-4856-ba2c-eaf4f7941b9c',
+    format: 'short',
+    category: 'podcast',
+  },
+  {
+    libraryId: '471301',
+    videoId: '98568701-fac8-4d59-a314-77f9b8dc8f83',
+    format: 'short',
+    category: 'podcast',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'edd1ee28-cd65-4959-9c22-9c27960e0bf3',
+    format: 'short',
+    category: 'animation',
+  },
+  {
+    libraryId: '471301',
+    videoId: '21e70614-cd1a-471c-9280-66276e8380ec',
+    format: 'short',
+    category: 'animation',
+  },
+  {
+    libraryId: '471301',
+    videoId: 'e91a844a-7970-4c87-9a3d-20643144c7c5',
+    format: 'short',
+    category: 'animation',
+  },
+    {
+    libraryId: '471301',
+    videoId: '419de9b5-7f54-4e6f-8ebf-cdda675d628f',
+    format: 'long',
+    category: 'documentary',
+  },
+  {
+    libraryId: '471301',
+    videoId: '4dfedd2d-d09c-4498-a245-e5b2eebff4b3',
+    format: 'long',
+    category: 'documentary',
+  },
+   {
+    libraryId: '471301',
+    videoId: '1eef1640-069f-4a23-9bf9-838a4badda80',
+    format: 'short',
+    category: 'brand podcast',
+  },
+   {
+    libraryId: '471301',
+    videoId: 'd43ec9e4-87b7-4704-b0c1-1fb5f7eec260',
+    format: 'short',
+    category: 'brand',
+  },
+    {
+    libraryId: '471301',
+    videoId: '2c59be36-b7c0-479b-a166-9a1a4b4e6d63',
+    format: 'long',
+    category: 'documentary talking-head',
+  },
+    {
+    libraryId: '471301',
+    videoId: '9272cb2d-b8f1-4f09-88df-88e1fd3090f3',
+    format: 'long',
+    category: 'brand',
+  },
+  
+  // …añade aquí cada vídeo en el orden que quieras que aparezca
+]
